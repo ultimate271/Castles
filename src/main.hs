d3f739fd08deff9302b66db1155eefbd6201d3bb
Brett Webster <bfireflash@gmail.com>
2017-12-17 14:01:56 -0600

    Refactored using stack

---
+++ src/main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Builtin
+
+main :: IO ()
+main = putStrLn "Not implemented"
--- tst/processing.hs
+++
@@ -1,7 +0,0 @@
-module ProcessingTest where
-
-import Processing
-import State
-import Action
-
---hexes $ doAction s _ == hexes s
